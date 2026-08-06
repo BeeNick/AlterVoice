2dd3deb0dcb5cb66ec9d3351efb40ce7fba9e866
BeeNick <nicola.fiini92@gmail.com>
2026-08-06 23:27:09 +0100

    Correzione indentazione

--- voice_anonymizer.py
+++ voice_anonymizer.py
@@ -477,14 +477,14 @@ class VoiceAnonymizer:
         self.board_off = Pedalboard([])
 
     def set_world_enabled(self, value: bool):
-    with self._lock:
-        self.world_enabled = value
-        self._session_cfg.enabled = value
+        with self._lock:
+            self.world_enabled = value
+            self._session_cfg.enabled = value
 
-        # quando passo a modalità legacy,
-        # sincronizzo il pitch corrente nel PitchShift
-        if not value:
-            self._build_boards()
+            # quando passo a modalità legacy,
+            # sincronizzo il pitch corrente nel PitchShift
+            if not value:
+              self._build_boards()
 
     def update_dsp_settings(self, hpf, lpf, comp_t, comp_r):
         with self._lock:
